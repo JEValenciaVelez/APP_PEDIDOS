d4ca347bbb81eb439a073bd6aee7755fa1b10565
unknown <juancho50301@gmail.com>
2023-09-25 11:39:09 -0500

    se completa crud en tablas empleados y usuarios

--- Api/src/routes/UserRoutes/index.js
+++ Api/src/routes/UserRoutes/index.js
@@ -4,10 +4,12 @@ const UserRoutes = express.Router()
 const postUser = require('../../controllers/UserControllers/postUser')
 const getUsers = require('../../controllers/UserControllers/getUsers')
 const deleteUser = require('../../controllers/UserControllers/deleteUser')
+const updateUser = require('../../controllers/UserControllers/updateUser')
 
 UserRoutes.post('/',postUser)
 UserRoutes.get('/', getUsers)
 UserRoutes.delete('/', deleteUser)
+UserRoutes.put('/', updateUser)
 
 
 module.exports = UserRoutes
